e032a483b86ae2923c6bf768eb248f24fe8eddb9
Sneha Roy <roycodescs@gmail.com>
2026-01-17 02:52:04 -0500

    fix: switch to pure HashRouter without basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom"; // Changed to HashRouter
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Resume from "./pages/Resume";
 import Composition from "./pages/Composition";
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* HashRouter handles the GitHub Pages 404 issue automatically */}
+      {/* REMOVE basename here - HashRouter doesn't need it on GH Pages */}
       <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
