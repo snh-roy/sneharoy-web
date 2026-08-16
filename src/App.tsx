5d8572f993733b7b79b3df570069de58d47ceb13
Sneha Roy <133825328+snh-roy@users.noreply.github.com>
2026-08-15 20:23:14 -0400

    remove unused files imported/created during learning vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Resume from "./pages/Resume";
 import Composition from "./pages/Composition";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
+// No basename here - HashRouter doesn't need it on GH Pages
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      {/* REMOVE basename here - HashRouter doesn't need it on GH Pages */}
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/composition" element={<Composition />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/composition" element={<Composition />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </HashRouter>
 );
 
 export default App;
